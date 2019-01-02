3a75e9e100fc23ea3860962bf98361e0c8e5b060
tandwysys <tandwysys@163.com>
2019-01-03 00:10:54 +0800

    新建spring-boot项目

---
+++ src/main/java/com/tdc/TdcProjectApplication.java
@@ -0,0 +1,14 @@
+package com.tdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdcProjectApplication.class, args);
+    }
+
+}
+
